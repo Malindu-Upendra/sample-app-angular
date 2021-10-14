2d789aff31a1c69f608b259536b0839aec904a4e
IT19074382 <it19074382@my.sliit.lk>
2021-10-14 18:15:27 +0530

    initial commmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sample-app-angular';
-}
+export class AppComponent {}
+
